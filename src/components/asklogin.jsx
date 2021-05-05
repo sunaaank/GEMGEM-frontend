3d1d6d86939c5bc507e1efd3b44b32ff13c01733
sunaaank <suna.namgung@gmail.com>
2021-05-05 23:54:14 +0900

    Fix : 미 로그인 상태 페이지 레이아웃 수정

--- src/components/asklogin.jsx
+++ src/components/asklogin.jsx
@@ -1,23 +1,36 @@
-import { List, ListItem } from "framework7-react";
+import { Button } from "framework7-react";
 import React from "react";
 
 const AskLogin = () => {
   return (
-    <List>
-      <ListItem
-        title="회원가입"
-        link="/users/sign_up"
-        icon="las la-question"
-        panelClose
-      />
-
-      <ListItem
-        title="로그인"
-        link="/users/sign_in"
-        icon="las la-question"
-        panelClose
-      />
-    </List>
+    <div className="flex flex-col items-center py-32">
+      <i id="ask_login" className="f7-icons text-gray-200">
+        person_crop_circle_badge_exclam
+      </i>
+      <p className="my-4">로그인 후 이용가능합니다</p>
+      <div className="pt-4 pb-10">
+        <div className="flex justify-center w-full">
+          <Button
+            large
+            outline
+            href="/users/sign_up"
+            color="black"
+            className="p-3 mr-4 px-10 border-black"
+          >
+            회원가입
+          </Button>
+          <Button
+            large
+            fill
+            href="/users/sign_in"
+            color="black"
+            className="p-3 px-10"
+          >
+            로그인
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
