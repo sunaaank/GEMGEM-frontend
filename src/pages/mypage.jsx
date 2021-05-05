6cf96de13d37461954a5f7f90b44ce534602ae65
sunaaank <suna.namgung@gmail.com>
2021-05-06 00:02:14 +0900

    Fix : 마이페이지 미 로그인 상태 페이지 레이아웃 수정

--- src/pages/mypage.jsx
+++ src/pages/mypage.jsx
@@ -16,17 +16,18 @@ import { logout } from "../common/api";
 import { getToken } from "../common/auth";
 import { toast, sleep } from "../js/utils";
 import ItemsSwiper from "../components/itemsSwiper.jsx";
+import AskLogin from "../components/asklogin.jsx";
 
 const MyPage = () => {
   const [itemsData, setItemsData] = useRecoilState(itemsDataState);
   let loggedIn = !!getToken().token;
-  const handleLogout = async () => {
-    f7.dialog.confirm("로그아웃하시겠습니까?", function () {
-      logout();
-      toast("로그아웃 되었습니다");
-      location.replace("/");
-    });
-  };
+  // const handleLogout = async () => {
+  //   f7.dialog.confirm("로그아웃하시겠습니까?", function () {
+  //     logout();
+  //     toast("로그아웃 되었습니다");
+  //     location.replace("/");
+  //   });
+  // };
 
   const onClickItem = (id) => {
     props.f7router.navigate(`/items/${id}/`);
@@ -35,35 +36,20 @@ const MyPage = () => {
   return (
     <Page name="mypage">
       <Navbar title="마이페이지" noHairline sliding={false} href="/" />
-      <div className="p-3 flex flex-col items-center">
-        <p>주문내역</p>
-        {/*<List>
-          <ListItem
-            title="회원가입"
-            link="/users/sign_up"
-            icon="las la-question"
-          ></ListItem>
-        </List>
-
-        <List>
-          {loggedIn ? (
-            <ListItem
-              title="로그아웃"
-              link="#"
-              icon="las la-question"
-              onClick={() => handleLogout()}
-            ></ListItem>
-          ) : (
-            <ListItem
-              title="로그인"
-              link="/users/sign_in"
-              icon="las la-question"
-            ></ListItem>
-          )}
-          </List>*/}
-      </div>
-      <div className="ml-4">
-        <ItemsSwiper itemsData={itemsData} onClickItem={() => onClickItem()} />
+      <div className="px-3 flex flex-col items-center">
+        {loggedIn ? (
+          <div>
+            <p>주문내역</p>
+            <div className="ml-4">
+              <ItemsSwiper
+                itemsData={itemsData}
+                onClickItem={() => onClickItem()}
+              />
+            </div>
+          </div>
+        ) : (
+          <AskLogin />
+        )}
       </div>
     </Page>
   );
